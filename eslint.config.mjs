24df0e3f389f0908640840953dd756c1db93ef7c
Ковалев Владислав <vlad.kovalev1503@gmail.com>
2024-09-20 09:59:51 +0300

    Удалил ненужный комментарий

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -2,13 +2,12 @@ import globals from "globals"
 import pluginJs from "@eslint/js"
 import tseslint from "typescript-eslint"
 
-
 export default [
   {
     files: ["**/*.{mjs,cjs,ts}"],
   },
   {
-    ignores: ["**/*.js"], // добавлено игнорирование js-файлов
+    ignores: ["**/*.js"],
   },
   { languageOptions: { globals: globals.browser } },
   {
